97a56592dd9b370915011fe24d6d495cb598aecf
michaeldoorhand <72215840+michaeldoorhand@users.noreply.github.com>
2023-04-05 20:11:06 -0500

    Update program3.rkt
    changed size function to be the length of the list instead of recursively going through the list and counting

--- program3.rkt
+++ program3.rkt
@@ -18,30 +18,30 @@
 ;if it doesn't have enough return a failure with the error message and stack
 ;else return the result of the operation in a success
 (define (safe-add stack)
-  (if (equal? (length stack) 1) (failure (list "ERROR: Not enough operands, " stack))
+  (if (equal? (length stack) 1) (failure (list "ERROR: Not enough operands" stack))
   (let ([x (last stack)]
         [y (last (pop stack))])
         (success (+ x y)))))
 
 (define (safe-subtract stack)
-  (if (equal? (length stack) 1) (failure (list "ERROR: Not enough operands, " stack))
+  (if (equal? (length stack) 1) (failure (list "ERROR: Not enough operands" stack))
   (let ([x (last stack)]
         [y (last (pop stack))])
         (success (- y x)))))
 
 (define (safe-multiply stack)
-  (if (equal? (length stack) 1) (failure (list "ERROR: Not enough operands, " stack))
+  (if (equal? (length stack) 1) (failure (list "ERROR: Not enough operands" stack))
   (let ([x (last stack)]
         [y (last (pop stack))])
         (success (* x y)))))
 
 ;Checks for division by 0 as well as enough operands
 (define (safe-div stack)
-  (if (<= (length stack) 1) (failure (list "ERROR: not enough operands, " stack))
+  (if (<= (length stack) 1) (failure (list "ERROR: not enough operands" stack))
   (let ([numerator (last (pop stack))]
         [denominator (last stack)])
     (cond
-      [(equal? denominator 0) (failure (list "ERROR: division by 0, " stack))]
+      [(equal? denominator 0) (failure (list "ERROR: division by 0" stack))]
       [else (success (/ numerator denominator))]))))
 
 ;Prints the current stack
@@ -57,13 +57,7 @@
 
 ;Prints the size of the stack
 (define (size stack)
-  (define (iter stack count)
-    (if (empty? stack) (display (string-append (number->string count) "\n"))
-    (let ([item (first stack)])
-      (cond
-        [(number? item) (iter (rest stack) (+ count 1))]
-        [else (iter (rest stack) count)]))))
-  (iter stack 0) stack)
+  (display (string-append (number->string (length stack)) "\n"))stack)
 
 ;Duplicate the top-most item on the stack
 (define (duplicate stack)
@@ -72,6 +66,7 @@
 ;Exits the program and prints the error and the current stack
 (define (end x)
   (display (first (from-failure #f x)))
+  (display ", current stack: ")
   (show (second (from-failure #f x)))
   (exit 0))
 
@@ -96,9 +91,9 @@
     [(equal? x "TOP")  (top stack)]
     [(equal? x "SIZ")  (size stack)]
     [(equal? x "DUP")  (duplicate stack)]
-    [(equal? x "END")  (end (failure (list "Program terminated, " stack)))]
+    [(equal? x "END")  (end (failure (list "Program terminated" stack)))]
     [(regexp-match #px"[+-]?\\d+(\\.\\d+)?" x) (push (string->number x) stack)]
-    [else (end (failure (list "Unknown command, " stack)))]))
+    [else (end (failure (list "Unknown command" stack)))]))
 
 ;Handles the user inputting multiple numbers or commands on the same line
 (define (handle-input input stack)
